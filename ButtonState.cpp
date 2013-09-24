ee1270221f7543e77272ab5d06c949bde26fe3c2
jpfaria <jpfaria@gmail.com>
2013-09-24 19:39:29 -0300

    Adding observer for when button is pressed

--- ButtonState.cpp
+++ ButtonState.cpp
@@ -1,1 +1,33 @@
- 
+#include "Arduino.h"
+#include "ButtonState.h"
+
+ButtonState::ButtonState(int pin)
+{
+  pinMode(pin, INPUT_PULLUP);
+  _pin = pin;
+  _state = HIGH;
+  _lastState = HIGH;
+  _startHold = 0;
+  _allow = false;
+}
+
+void ButtonState::observer()
+{
+  _state = digitalRead(_pin);
+
+  if (_state == LOW && _lastState == HIGH){  
+    _startHold = millis();                       
+    _allow = true;                                
+  }
+
+  if (_allow == true && _state == LOW && _lastState == LOW){
+    if ((millis() - _startHold) >= 20){
+      Serial.println(_pin);
+      //rlyState = !rlyState;
+      _allow = false;
+    }
+  } 
+
+  _lastState = _state;
+  
+}
--- ButtonState.h
+++ ButtonState.h
@@ -1,1 +1,19 @@
- 
+#ifndef ButtonState_h
+#define ButtonState_h
+
+#include "Arduino.h"
+
+class ButtonState
+{
+  public:
+    ButtonState(int pin);
+    void observer();
+  private:
+    int _pin;
+    int _state;
+    int _lastState;
+    long _startHold;
+    boolean _allow;
+};
+
+#endif
